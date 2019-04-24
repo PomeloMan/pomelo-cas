4c82e4abdb2c8a3df92d7bdd243840ef1e3d7484
PomeloMan <fengchao.z@outlook.com>
2019-04-24 13:08:37 +0800

    Commit

--- pomelo-cas-client/src/main/java/pomelo/cas/client/core/Application.java
+++ pomelo-cas-client/src/main/java/pomelo/cas/client/core/Application.java
@@ -1,9 +1,12 @@
 package pomelo.cas.client.core;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +15,22 @@ import net.unicon.cas.client.configuration.EnableCasClient;
 
 @SpringBootApplication
 @EnableCasClient
-@RequestMapping()
+@Controller
+@RequestMapping("/cas-client")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/cas-client/{username}")
+	@GetMapping("/{username}")
 	public ResponseEntity<?> info(@PathVariable String username) {
 		return new ResponseEntity<>(username, HttpStatus.OK);
+	}
+
+	@RequestMapping("/logout")
+	public String loginOut(HttpSession session) {
+		session.invalidate();
+		return "redirect:http://localhost:8080/cas/logout";
 	}
 }
